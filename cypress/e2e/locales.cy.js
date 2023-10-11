04a1e8d0dedc2f5ddb516ed9f34a78b6376d4d20
Augusto Rufino <joseaorufino@gmail.com>
2023-10-10 22:53:06 -0300

    test: add more tests in cypress for locales

--- cypress/e2e/locales.cy.js
+++ cypress/e2e/locales.cy.js
@@ -78,7 +78,7 @@ describe('locales', () => {
   });
 
   // Test 6
-  it.only('should show a success message when edit a locale', () => {
+  it('should show a success message when edit a locale', () => {
     cy.clickInFirst('a[href="/admin/locales/"]');
 
     cy.get('*[class^="ui labeled icon button "]').last().click();
@@ -90,5 +90,67 @@ describe('locales', () => {
     cy.get('*[class^="close icon"]').last().click();
 
     cy.findByText('Locale has been successfully updated.').should('not.be.visible');
+  });
+
+  // Test 7
+  it('should order by Name in descending order after filter with contains', () => {
+    cy.clickInFirst('a[href="/admin/locales/"]');
+
+    cy.findByRole('link', { name: /Clear filters/i }).click();
+
+    cy.get('*[id="criteria_code_value"]').type('e');
+
+    cy.findByRole('button', { name: 'Filter' }).click();
+
+    cy.findByRole('columnheader', { name: 'Name' }).click();
+
+    cy.findByRole('columnheader', { name: 'Name' }).click();
+
+    cy.get('tbody tr.item:first-child td:nth-child(2)').should('contain', 'es_MX Spanish (Mexico)');
+  });
+
+  // Test 8
+  it('should order by Name in descending order after filter with not contains', () => {
+    cy.clickInFirst('a[href="/admin/locales/"]');
+
+    cy.get('*[id="criteria_code_type"]').select('not_contains');
+
+    cy.get('*[id="criteria_code_value"]').type('a');
+
+    cy.findByRole('button', { name: 'Filter' }).click();
+
+    cy.findByRole('columnheader', { name: 'Name' }).click();
+
+    cy.findByRole('columnheader', { name: 'Name' }).click();
+
+    cy.get('tbody tr.item:first-child td:nth-child(2)').should('contain', 'zh_CN Chinese (China)');
+  });
+
+  // Test 9
+  it('should filter locales by starts with filter', () => {
+    cy.clickInFirst('a[href="/admin/locales/"]');
+
+    cy.get('*[id="criteria_code_type"]').select('starts_with');
+
+    cy.get('*[id="criteria_code_value"]').type('e');
+
+    cy.findByRole('button', { name: 'Filter' }).click();
+
+    cy.findByText('Polish (Poland)').should('not.exist');
+    cy.get('body').should('contain', 'en_US English (United States)');
+  });
+
+  // Test 10
+  it('should filter locales by not in filter', () => {
+    cy.clickInFirst('a[href="/admin/locales/"]');
+
+    cy.get('*[id="criteria_code_type"]').select('not_in');
+
+    cy.get('*[id="criteria_code_value"]').type('pt_pt');
+
+    cy.findByRole('button', { name: 'Filter' }).click();
+
+    cy.findByText('Portuguese (Portugal)').should('not.exist');
+    cy.get('body').should('contain', 'fr_FR French (France)');
   });
 });
